b3b7f5d52f52ddf84a7008dd2f0f63dd40b4466c
isalykke <i@lykkeh.dk>
2019-10-24 10:39:32 +0200

    bs script successfully downloads all txt files from security now. now in the folder /txts

--- beautifulsoup_test.py
+++ beautifulsoup_test.py
@@ -1,27 +1,41 @@
 #code from: https://stackoverflow.com/questions/19056031/download-files-using-requests-and-beautifulsoup
 import requests
 from bs4 import BeautifulSoup as bs
-import urllib
 import re
+import urllib
+import os
 
 def get_valid_filename(s):
     s = str(s).strip().replace(' ', '_')
     return re.sub(r'(?u)[^-\w.]', '', s)
 
-_URL = "https://www.grc.com/securitynow.htm"
+
+
+_URL = "https://www.grc.com/securitynow.htm" #current site
+_URLARCHIVE = "https://www.grc.com/sn/past/year.htm" #generic site for older episodes
+_ALLURLs = []
+
+for i in range(2005, 2018):
+    _ALLURLs.append(_URLARCHIVE.replace("year", str(i)))
+
+_ALLURLs.append(_URL) #append newest episodes to list of older episodes
 
 # functional
-r = requests.get(_URL)
-soup = bs(r.text, features="lxml")
 urls = []
 names = []
-for i, link in enumerate(soup.findAll('a')):
-    _FULLURL = _URL + str(link.get('href'))
-    if _FULLURL.endswith('.txt'):
-        urls.append(_FULLURL.replace("securitynow.htm/", "")) #remove "securitynow.htm/" to get to the right page for txt files
-        names.append(soup.select('a')[i].attrs['href'])
+
+for url in _ALLURLs:
+    r = requests.get(url)
+    soup = bs(r.text, features="lxml")
+    for i, link in enumerate(soup.findAll('a')):
+        _FULLURL = url + str(link.get('href'))
+        if _FULLURL.endswith('.txt'):
+            urls.append("https://www.grc.com" + soup.select('a')[i].attrs['href']) #remove "securitynow.htm/" from url to get to the right page for txt files
+            names.append(soup.select('a')[i].attrs['href'])
 
 names_urls = zip(names, urls)
+
+os.chdir("/Users/isalykkehansen/OneDrive/uni_kandidat/natural_language_processing_NLP/NLPexam/txts") #change directory to txts folder for saving files
 
 for name, url in names_urls:
     print(name, url)
@@ -29,4 +43,7 @@ for name, url in names_urls:
     txt = open(get_valid_filename(name), 'wb')
     txt.write(res.read())
     txt.close()
+
+os.chdir("/Users/isalykkehansen/OneDrive/uni_kandidat/natural_language_processing_NLP/NLPexam/")
+
 
